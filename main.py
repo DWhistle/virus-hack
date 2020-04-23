beaa7bdc03a9e4328d84c2bc2efea2550b4bc596
Andrey Shibaev <dwhistle.dev@gmail.com>
2020-04-23 22:56:10 +0300

    flask init

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from private.config import configure_resources
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hw():
+    return {"123": "123"}
 
 if __name__ == '__main__':
     configure_resources()
+    app.run("0.0.0.0", port=5000)
