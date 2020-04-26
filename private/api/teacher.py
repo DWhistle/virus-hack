5aa00611bf34853c92d0a0296ac85a193c4189eb
Andrey Shibaev <dwhistle.dev@gmail.com>
2020-04-26 21:42:22 +0300

    tests

--- private/api/teacher.py
+++ private/api/teacher.py
@@ -1,9 +1,7 @@
 from flask import Blueprint
-from private.db.models import Teacher
 
 teacher_api = Blueprint("teacher", __name__, url_prefix="/teacher")
 
 @teacher_api.route("/", methods = ["GET"])
-def get_students():
-    teacher = Teacher()
-    return {"teachers": teacher.get_all()}
+def get_teachers():
+    return {"teachers": 123}
