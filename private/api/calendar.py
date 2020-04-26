bb2873c99ac3b309263bf0b5d21351d900a4a9c8
Andrey Shibaev <dwhistle.dev@gmail.com>
2020-04-26 23:15:35 +0300

    filling for calendars

--- private/api/calendar.py
+++ private/api/calendar.py
@@ -15,10 +15,8 @@ def get_by_id():
     events = []
     with init_session() as ss:
         q = ss.query(Event)
-        q.join(User)
-        q.filter(User.id == Event.teacher_id)
-        q.join(Event)
-        q.filter(Lesson.id == Event.lesson_id)
+        q.join(User, User.id == Event.teacher_id)
+        q.join(Lesson, Lesson.id == Event.lesson_id)
         if event_id:
             q.filter(Event.id == event_id)
         if class_id:
