880e2a3fd4c00ddc01b98ea54beae5ab20a2ec9b
Andrey Shibaev <dwhistle.dev@gmail.com>
2020-04-26 22:59:12 +0300

    filling for calendars

--- tests/base_test.py
+++ tests/base_test.py
@@ -3,9 +3,20 @@ from private.db.models import Profile, User, Event, Class, init_session, Lesson
 import datetime
 from datetime import datetime
 from datetime import timedelta
+from random import randint
 
 class TestEdu(unittest.TestCase):
-    
+
+    def add_multiple_calendars(self):
+        with init_session() as ss:
+            for i in range(2, 10):
+                day = randint(1, 7)
+                event = Event(id=i,teacher_id=1,lesson_id=1,
+                class_id=1,description='Урок ' + str(i), 
+                begin_time = datetime.now() + timedelta(days=day),
+                end_time = datetime.now() + timedelta(days=day) + timedelta(minutes=40))
+                ss.add(event)
+
     def test_db_tables(self):
         def_class = Class(id=1, grade=1, roles=123, specialization='A')
         def_user = User(id=1, name = "Ольга Петровна", username='123', password='123', class_id = 1)
