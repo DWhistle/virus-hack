06fd07c7723abffbfb3158e670ed3fda66d1408b
Andrey Shibaev <dwhistle.dev@gmail.com>
2020-04-27 22:14:15 +0300

    more db

--- private/api/assessment.py
+++ private/api/assessment.py
@@ -1,17 +1,21 @@
 from flask import Blueprint, request
 from flask.helpers import send_file
 import io
-from PIL import Image
 
 assessment_api = Blueprint("assessment", __name__, url_prefix="/assessment")
 
-@assessment_api.route("/", methods = ["GET"])
-def get_by_id():
-    id = int(request.args.get('id') or 0)
+@assessment_api.route("/<id>", methods = ["GET"])
+def get_by_id(id):
+    id = int(id or 0)
     f_name = f"../pictures/{id}.jpg"
-    image_binary = Image.open(f_name)
     return send_file(
-    io.BytesIO(image_binary),
+    f_name,
     mimetype='image/jpeg',
-    as_attachment=True,
-    attachment_filename=f_name)
+    as_attachment=True)
+
+@assessment_api.route("/pins/<id>", methods = ["GET"])
+def pins_by_id(id):
+    id = int(id or 0)
+    return {"pins": 
+    [(500,700, "вот тут поправить"),
+    (550,800, "вот тут ещё")]}
