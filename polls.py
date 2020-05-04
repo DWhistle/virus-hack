6d732a2927dab0327d13563b35b5361ac276b0a4
Andrey Shibaev <dwhistle.dev@gmail.com>
2020-05-04 18:00:45 +0300

    polling server

--- polls.py
+++ polls.py
@@ -3,29 +3,31 @@ import socketio
 from private.config import Configurator
 Configurator.configure_resources()
 from private.db.models.education import DbMethods
+import json
 
 sio = socketio.Server(cors_allowed_origins='*')
 app = socketio.WSGIApp(sio)
 
 
 
-@sio.on('sendpoll')
+@sio.on('insertpoll')
 def insert_poll(uid, data):
-    print('sendpoll ', data)
-
-
-
-@sio.on('getpoll')
-def get_poll(data):
     print(data)
-    sio.emit('response')
+    poll = json.loads(data)
+    print('insertpoll ', poll)
+
+
+
+@sio.on('publishpoll')
+def publish_poll(uid, data):
+    sio.emit('publish', data)
 
 @sio.event
 def connect(sid, environ):
-    print('connect ', sid)
+    print('connected ', sid)
 
 @sio.event
 def disconnect(sid):
-    print('disconnect ', sid)
+    print('disconnected ', sid)
 
 eventlet.wsgi.server(eventlet.listen(('', 5555)), app)
