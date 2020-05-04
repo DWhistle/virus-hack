51b162e0b93ef5016b2ead869260d51566eea6ea
Andrey Shibaev <dwhistle.dev@gmail.com>
2020-05-04 16:26:12 +0300

    polling server

--- polls.py
+++ polls.py
@@ -14,5 +14,11 @@ def get_poll(data):
     print(data)
     sio.emit('response')
 
+@sio.on('disconnect')
+def disconnected():
+    print('disconnected')
+@sio.on('connect')
+def connected():
+    print('connected')
 
 eventlet.wsgi.server(eventlet.listen(('', 5555)), app)
